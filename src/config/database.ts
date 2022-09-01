09eae0513151dd951ed37e535cd5b7aa6125e35d
Tanaca <jtanacar@gmail.com>
2022-09-01 04:18:14 -0300

    chore: change sqlite to postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,8 +3,13 @@ import { DataSource } from "typeorm";
 import models from "@models";
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "./database.sqlite",
+    type: "postgres",
+    database: "orange-notes",
+    name: "default",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "123",
     synchronize: true,
     entities: models,
 });
